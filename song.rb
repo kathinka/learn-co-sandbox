6d9b6cceb9e957857d950f54e1a0c4a976b65c3a
kathinka martinsen <kathinkamartinsen@gmail.com>
2020-11-15 12:49:36 +0000

    Automatically backed up by Learn

---
+++ song.rb
@@ -0,0 +1,22 @@
+class Song
+ 
+  @@all = []
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+  
+  def self.print_all_song_names
+ 
+   all.name
+  end
+end
+  
+end
